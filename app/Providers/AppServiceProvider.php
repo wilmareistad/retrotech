23ad5781a12b70475a3844ce8eb3bca97fc58179
Wilma <wilmareistad@icloud.com>
2026-03-14 21:14:43 +0100

    https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\Brand;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
